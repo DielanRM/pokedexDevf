d1973cd3acbd250c7b3026d70e4c73bc8fc990f3
DielanRM <dielan.r.m@gmail.com>
2024-05-07 18:49:47 -0500

    error en el html corregido, se agrego type module en el script

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-import { callPokemons, pokemons } from "./src/controllers/apiCall.js";
 import { listaNueva } from "./src/view/vista.js";
+// import { pokemons } from "./src/controllers/apiCall.js";
+
+
+const searchInput = document.getElementById("search-input")
+const searchButton = document.getElementById("searchButton")
+const listapokemons = document.getElementById("lista")
 
 
 
-
-
-console.log(`estamos dentro ${pokemons}`);
-console.log('+++++++++++++++++++++++++++++++++++', listaNueva);
+searchButton.addEventListener('click', function(){
+    listapokemons.innerHTML = "<P> Lentos pero seguros avanzando fuerte</P>"
+})
+// console.log(`estamos dentro ${pokemons}`);
+console.log(listaNueva);
 
 
--- src/view/vista.js
+++ src/view/vista.js
@@ -4,18 +4,18 @@ import Pokemon from "../models/classPokemon.js";
 
 async function crearLista() {
      var listaNueva = []
-    const nuevaLista = pokemons.map(async function (pokemon) {
+
+    for (let i = 0; i < pokemons.length; i++){
+        let pokemon = pokemons[i]
         let nombre = pokemon.name
         let tipo = await pokemon.types[0].type.name
         let peso = pokemon.weight
         let imagen = pokemon.sprites.front_default
-        let movimientos = await pokemon.moves.map((moves)=>{moves.move.name})
+        let movimientos = await pokemon.moves.map(moves=> moves.move.name)
 
         let nuevoPokemon = new Pokemon(nombre, tipo, peso, imagen, movimientos)
         listaNueva.push(nuevoPokemon)
-    
-        return nuevaLista
-    })
+    }
     return listaNueva
 }
 
