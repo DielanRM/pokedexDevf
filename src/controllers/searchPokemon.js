f94639de0a00145fc51994dc67711b30465d4d76
Diego Alan <135179237+DielanRM@users.noreply.github.com>
2024-05-18 15:41:18 -0600

    estilos y busqueda  listos

--- src/controllers/searchPokemon.js
+++ src/controllers/searchPokemon.js
@@ -14,21 +14,26 @@ async function busqueda () {
         console.log(encontrado[index]);
 
 
-        const pokeCard = document.createElement('div')    
-        pokeCard.classList.add('pokeCards')
-        let movimientos = encontrado[index].movimientos//.map(movimiento => movimiento.nombre)//.join(', ')
-    
-        
+        const pokemonInfo = document.createElement('div')    
+        pokemonInfo.classList.add('pokeInfo')
+        let movimientos = encontrado[index].movimientos    
 
         console.log(movimientos);
-        pokeCard.innerHTML = `
+        pokemonInfo.innerHTML = `
+        <div class= "info">   
+        <img src= '${encontrado[index].imagen}' height = 400px width=300px />    
         <h2>Nombre: ${encontrado[index].nombre}</h2>
         <h3>Tipo: ${encontrado[index].tipo}</h3>
         <p>Peso: ${encontrado[index].peso}</p>
-        <img src= '${encontrado[index].imagen}' height = 300px width=200px />
-        <p>Movimientos: ${movimientos.join(',   ')}</p>
+        </div>
+
+        
+        <div class= "info">
+        <p>MOVIMIENTOS: </p>
+        <p>${movimientos.join(',    -')}</p>
+        </div>
         `
-        listapokemons.appendChild(pokeCard)
+        listapokemons.appendChild(pokemonInfo)
     }
 }
 
