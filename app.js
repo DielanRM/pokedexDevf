da8f62c10b2e532c0e25e9d53adafba586d8aa4a
Diego Alan <135179237+DielanRM@users.noreply.github.com>
2024-05-17 21:02:45 -0600

    muestreo de tarjetas funcionando

--- app.js
+++ app.js
@@ -10,8 +10,27 @@ const listapokemons = document.getElementById("lista")
 
 searchButton.addEventListener('click', function(){
     listapokemons.innerHTML = "<P> Lentos pero seguros avanzando fuerte</P>"
+
 })
 // console.log(`estamos dentro ${pokemons}`);
+
 console.log(listaNueva);
+
+
+listaNueva.forEach(element => {
+    const pokeCard = document.createElement('div')    
+    pokeCard.classList.add('pokeCards')
+    //const movimientos = movimientos.map(key => element.movimientos[key]).join()
+// <p>Movimientos: ${movimientos}</p>
+    pokeCard.innerHTML = `
+    <h2>Nombre: ${element.nombre}</h2>
+    <h3>Tipo: ${element.tipo}</h3>
+    <p>Peso: ${element.peso}</p>
+   
+    <img src= '${element.imagen}' height = 300px width=200px />
+    `
+
+    listapokemons.appendChild(pokeCard)
+});
 
 
