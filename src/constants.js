26f612470fcfb1f87bd7cc64941c134c587a9624
Diego Alan <135179237+DielanRM@users.noreply.github.com>
2024-05-18 13:45:22 -0600

    muestra los movimientos del pokemon buscado

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
-const searchInput = document.getElementById("search-input").value
 const searchButton = document.getElementById("searchButton")
 const listapokemons = document.getElementById("lista")
 
 
-export {searchInput, searchButton, listapokemons}
+export { searchButton, listapokemons}
--- src/controllers/searchPokemon.js
+++ src/controllers/searchPokemon.js
@@ -1,13 +1,35 @@
 import { listapokemons } from "../constants.js";
 import { listaNueva } from "../view/vista.js";
 
-const searchInput = document.getElementById("search-input").value
 
-function busqueda () {
+async function busqueda () {
+    let nombreInput = document.getElementById("search-input").value
+    let exist = listaNueva.some(nombrePokemon => nombrePokemon.nombre === nombreInput )
     listapokemons.innerHTML = '';
-    listapokemons.innerHTML = `<p>ta taaa ${searchInput}</p>`
-    console.log("-------------------------------------------------");
-    console.log(listaNueva);
+
+    if (exist) {
+        let encontrado = listaNueva.filter(name => name.nombre === nombreInput)
+        let index = encontrado.findIndex(indice => indice.nombre === nombreInput)
+        console.log("=================================");
+        console.log(encontrado[index]);
+
+
+        const pokeCard = document.createElement('div')    
+        pokeCard.classList.add('pokeCards')
+        let movimientos = encontrado[index].movimientos//.map(movimiento => movimiento.nombre)//.join(', ')
+    
+        
+
+        console.log(movimientos);
+        pokeCard.innerHTML = `
+        <h2>Nombre: ${encontrado[index].nombre}</h2>
+        <h3>Tipo: ${encontrado[index].tipo}</h3>
+        <p>Peso: ${encontrado[index].peso}</p>
+        <img src= '${encontrado[index].imagen}' height = 300px width=200px />
+        <p>Movimientos: ${movimientos.join(',   ')}</p>
+        `
+        listapokemons.appendChild(pokeCard)
+    }
 }
 
 export { busqueda }
